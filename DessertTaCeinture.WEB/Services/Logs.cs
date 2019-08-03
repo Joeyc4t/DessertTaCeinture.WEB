2d0c54a377a6f7609da192bf88bd47d718448638
Joey Boeme <joey.boeme@hotmail.fr>
2019-08-03 14:40:49 +0200

    Init logs management

---
+++ DessertTaCeinture.WEB/Services/Logs.cs
@@ -0,0 +1,51 @@
+﻿using DessertTaCeinture.WEB.Models.Logs;
+using DessertTaCeinture.WEB.Tools;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace DessertTaCeinture.WEB.Services
+{
+    public class Logs
+    {
+        #region Instances
+        private static Logs _Instance;
+        public static Logs Instance
+        {
+            get { return _Instance = _Instance ?? new Logs(); }
+        }
+        private Logs() { }
+        #endregion
+
+        public IEnumerable<LogsModel> GetAll()
+        {
+            IEnumerable<LogsModel> models = Enumerable.Empty<LogsModel>();
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(StaticValues.BASE_URI);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(StaticValues.API_MEDIA_TYPE));
+
+                    HttpResponseMessage Res = client.GetAsync($"api/Logs").Result;
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        var result = Res.Content.ReadAsStringAsync().Result;
+                        models = JsonConvert.DeserializeObject<IEnumerable<LogsModel>>(result);
+                    }
+                }
+                return models.OrderByDescending(m => m.LogDate);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
